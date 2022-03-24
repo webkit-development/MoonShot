d0c6aaa16f4c7836f1baf7446a7afc204432392b
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-24 08:09:55 -0400

    Added the main contentview

--- MoonShot/Bundle-Decodable.swift
+++ MoonShot/Bundle-Decodable.swift
@@ -9,7 +9,7 @@ import Foundation
 
 
 extension Bundle {
-    func decode(_ file: String) -> [String: Astronaut] {
+    func decode<T: Codable>(_ file: String) -> T {
         guard let url = self.url(forResource: file, withExtension: nil) else {
                 fatalError("Failed to locate \(file) in bundle.")
         }
@@ -17,8 +17,11 @@ extension Bundle {
             fatalError("Failed to load \(file) from bundle")
         }
         let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
         
-        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode \(file) from bundle.")
         }
         return loaded
