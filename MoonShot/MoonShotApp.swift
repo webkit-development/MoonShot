e3aecc8d592635563219e867781e6e93409d8281
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-24 03:34:17 -0400

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Kevin Stradtman on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
